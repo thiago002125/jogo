d0cb940e0437963baabbd333f21d7a5541fbf3c6
thiago002125 <102823398+thiago002125@users.noreply.github.com>
2022-06-15 16:19:15 -0300

    Update main.py

--- jogo/main.py
+++ jogo/main.py
@@ -10,7 +10,7 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+pygame.display.set_caption("FIGHT GAYMER")
 
 #Framerate
 clock = pygame.time.Clock()
